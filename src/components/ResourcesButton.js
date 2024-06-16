48c270d450d4f98154dbaa3573cb0e3cf107a170
Rohan Rattan <83845975+RohanR24@users.noreply.github.com>
2024-06-16 16:50:57 -0700

    Update ResourcesButton.js

--- src/components/ResourcesButton.js
+++ src/components/ResourcesButton.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+/*import { useState } from 'react';
 import styles from '../css/ResourcesButton.module.css';
 
 function ResourcesButton({ button, content, state }) {
@@ -24,6 +24,6 @@ function ResourcesButton({ button, content, state }) {
       </div>
     </div>
   );
-}
+}*/ 
 
 export default ResourcesButton;
